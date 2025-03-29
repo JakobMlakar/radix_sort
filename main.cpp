5c70c88a98f4c4915a9a75fa40caab53afebbf22
Jakob Mlakar <ninjabots2019@gmail.com>
2025-03-29 16:23:29 +0100

    Dodan error-handling za branje iz datotekte

--- main.cpp
+++ main.cpp
@@ -6,12 +6,23 @@ using namespace std;
 bool Branje_Stevil(vector<unsigned char>& vec, const char s[]) {
     ifstream input(s);
     int st;
+    
     if (!input.is_open()) {
+        cerr << "Error: Could not open file " << s << endl;
         return false;
     }
+    
     while (input >> st) {
+        if (st < 0 || st > 255) {
+            cerr << "Warning: Number " << st << " out of range, truncating to fit unsigned char" << endl;
+        }
         vec.push_back(static_cast<unsigned char>(st));
     }
+    
+    if (vec.empty()) {
+        cerr << "Warning: No valid numbers found in file" << endl;
+    }
+    
     input.close();
-    return true;
+    return !vec.empty();
 }
